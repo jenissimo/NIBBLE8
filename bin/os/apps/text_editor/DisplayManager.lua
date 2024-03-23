9e9e6b795c699db45e0f9aa48157cc47aaae9e39
Eugeniy Smirnov <jenissimo@gmail.com>
2024-03-23 19:38:22 +0300

    Fix text draw in TextEditor

--- bin/os/apps/text_editor/DisplayManager.lua
+++ bin/os/apps/text_editor/DisplayManager.lua
@@ -10,18 +10,23 @@ end
 
 function DisplayManager:redrawText(textEditor)
     local firstLineIndex = math.max(1, math.floor(textEditor.scroll.y) + 1)
+    local lastLineIndex = firstLineIndex + textEditor.rows_on_screen - 1
     local startY = textEditor.y + textEditor.offsetY
     local lineHeight = 6 -- Height of each line of text
     local charWidth = 4 -- Width of each character
     local xPos = textEditor.x - textEditor.scroll.x * charWidth
+    local index = 1
 
-    for lineIndex, drawText in ipairs(textEditor.drawLines) do
-        local yOffset = ((lineIndex + firstLineIndex - 1) - firstLineIndex) * lineHeight + startY
-        local cursorDocumentY = textEditor.cursor.y + textEditor.scroll.y
-        if lineIndex == cursorDocumentY + 1 then
-            drawText = textEditor:cacheLine(lineIndex)
-        end
+    for lineIndex = firstLineIndex, lastLineIndex do
+        local yOffset = (lineIndex - firstLineIndex) * lineHeight + startY
+        local drawText = textEditor.drawLines[index]
+        --local cursorDocumentY = textEditor.cursor.y + textEditor.scroll.y
+        --if lineIndex == cursorDocumentY + 1 then
+            --drawText = textEditor:cacheLine(lineIndex)
+        --end
+        if drawText == nil then break end
         print(drawText, xPos, yOffset)
+        index = index + 1
     end
 end
 
