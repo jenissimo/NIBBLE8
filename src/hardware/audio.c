32a20ef53dbb2fa93fd00f9e0b0005250ab6709f
Eugeniy Smirnov <jenissimo@gmail.com>
2024-04-07 20:24:15 +0300

    Start work on new tracker

--- src/hardware/audio.c
+++ src/hardware/audio.c
@@ -7,6 +7,60 @@ float freq;
 bool note_on;
 Instrument currentInstrument;
 
+/*
+    // print first pattern in hex
+    printf("First pattern in hex: \n");
+    static const char *NOTE_STRING[12] = {"C-", "C#", "D-", "D#", "E-", "F-", "F#", "G-", "G#", "A-", "A#", "B-"};
+    for (int line = 0; line < 64; line++)
+    {
+        int pos = (context.order[0] * 64 + line) * context.num_channels * 4;
+        unsigned char (*data)[4] = (unsigned char(*)[4]) (context.patterns + pos);
+
+        printf("%02x ", line);
+
+        for (int i = 0; i < context.num_channels; i++)
+        {
+            int sample = (data[i][0] & 0xf0) | (data[i][2] >> 4);
+            int period = ((data[i][0] & 0x0f) << 8) | data[i][1];
+            int effect = ((data[i][2] & 0x0f) << 8) | data[i][3];
+            int note = _pocketmod_period_to_note(period);
+            int noteIndex = note % 12;
+            int octave = note / 12 + 3;
+
+            //printf("%d", note);
+
+            if (note <= 0)
+            {
+                printf("---");
+            }
+            else
+            {
+                printf("%s%d", NOTE_STRING[noteIndex], octave);
+            }
+
+            if (sample > 0)
+            {
+                printf("%02X", sample);
+            }
+            else
+            {
+                printf("--");
+            }
+
+            if (effect > 0)
+            {
+                printf("%03X ", effect);
+            }
+            else
+            {
+                printf("--- ");
+            }
+        }
+
+        printf("\n");
+    }
+*/
+
 void nibble_audio_update(int16_t *stream, int num_samples)
 {
     for (int i = 0; i < num_samples; ++i)
